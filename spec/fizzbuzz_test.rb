fcd8bbdb1427a2d6d7f9d48e73a0760c7520190e
Alejandro Reyes <alejandroereyes@gmail.com>
2015-05-18 17:25:07 -0500

    added test for number to be returned appropriately

--- spec/fizzbuzz_test.rb
+++ spec/fizzbuzz_test.rb
@@ -13,20 +13,27 @@ class FizzBuzzTest < Test::Unit::TestCase
    num = 3 # Given num is 3
    play = FizzBuzz.new # When num is divisible by 3?
    result = play.fizz(3)
-   assert_equal("fizz",result) # then expect value
+   assert_equal("fizz",result) # Then expect value to be fizz
   end
 
   def test_FizzBuzz_method_returns_buzz
     num = 5 # Given num is 5
     play = FizzBuzz.new # When num is divisible by 5?
     result = play.buzz(num)
-    assert_equal("buzz",result)
+    assert_equal("buzz",result) # Then expect value to be buzz
   end
 
   def test_FizzBuzz_method_returns_fizzbuzz
     num = 15 # Given num is 15
     play = FizzBuzz.new # When num is divisible by 3 & 5?
     result = play.fizzbuzz(num)
-    assert_equal("fizzbuzz",result)
+    assert_equal("fizzbuzz",result) # Then expect value to be fizzbuzz
+  end
+
+  def test_number_method_returns_a_number
+    num = 8 # Given num is 8
+    play = FizzBuzz.new # when num isn't divisible by 3 or 5
+    result = play.number(num)
+    assert_equal(8,result) # Then expect value to be a 8
   end
 end # class
